40cd2e621c0e42f8c3a145a285603b8a8ab1652a
Ajharul Haque Choudhury <69278033+Ajharul7@users.noreply.github.com>
2023-01-02 12:07:04 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 
 const Connection = async(URL) => {
+    let URL = "mongodb+srv://ajharul7:ajharul1234@cluster0.c94fagc.mongodb.net/?retryWrites=true&w=majority";
     try {
         await mongoose.connect(URL,{useNewUrlParser : true,useUnifiedTopology: true});
         console.log("database connect");
